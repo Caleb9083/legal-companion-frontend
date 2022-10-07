6e26c5a0480cad5cf38bf187c8f250aed85fccbc
Caleb9083 <=calebosam12@gmail.com>
2022-10-07 11:22:28 +0000

    Icon added to view constituion button

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,21 +1,26 @@
-import React from 'react'
-import "./styles/Hero.css"
-import { Button } from "./Button"
+import React from "react";
+import "./styles/Hero.css";
+import { Button } from "./Button";
+import { AiFillEye } from 'react-icons/ai';
 
 const Hero = () => {
     return (
-        <div className='hero_container'>
-            <div className='hero_left'>
-                <span>Want to know</span>
-                <span>what some</span>
-                <span>constitutions</span>
+        <div className="hero_container">
+            <div className="hero_left">
+                <div className="span_elements">
+                    <span>Want to know</span>
+                    <span>what some</span>
+                    <span>constitutions</span>
+                </div>
                 <div className="button_container">
-                    <Button buttonSize={"btn--medium"}>View Constitutions</Button>
+                    <Button buttonSize={"btn--medium"}><AiFillEye />View Constitutions</Button>
                 </div>
             </div>
-            <div className='hero_right'></div>
-        </div>
-    )
-}
+            <div className="hero_right">
 
-export default Hero
+            </div>
+        </div>
+    );
+};
+
+export default Hero;
