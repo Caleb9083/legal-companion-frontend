5b2c19a8f74bcb24c5b369e427a712d89eb5fcbd
Caleb9083 <=calebosam12@gmail.com>
2022-10-07 15:32:57 +0000

    Constitution Page files created and to be implemented

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from './pages/Home';
 import Footer from './components/Footer';
+import Constitution from './pages/Constitution';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <div className="pages">
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/constitutions" element={<Constitution />} />
           </Routes>
         </div>
       </BrowserRouter>
---
+++ src/pages/Constitution.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./styles/Constitution.css"
+
+const Constitution = () => {
+    return (
+        <div>Constitution</div>
+    )
+}
+
+export default Constitution
