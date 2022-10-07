ab80c89411246ebfc9ab76cf9144c292a2d66c5e
Caleb9083 <=calebosam12@gmail.com>
2022-10-07 10:58:37 +0000

    Button component created

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from 'react';
+import './styles/Button.css';
+import { Link } from 'react-router-dom';
 
-const Button = () => {
+
+const SIZES = ['btn--small', 'btn--medium', 'btn--large'];
+
+export const Button = ({
+    children,
+    type,
+    onClick,
+    buttonSize,
+    route
+}) => {
+
+    const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
+
     return (
-        <div>Button</div>
-    )
-}
-
-export default Button
+        <Link to={`${route}`} className='btn-mobile'>
+            <button
+                className={`btn  ${checkButtonSize}`}
+                onClick={onClick}
+                type={type}
+            >
+                {children}
+            </button>
+        </Link>
+    );
+};
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import "./styles/Hero.css"
+import { Button } from "./Button"
 
 const Hero = () => {
     return (
@@ -8,7 +9,9 @@ const Hero = () => {
                 <span>Want to know</span>
                 <span>what some</span>
                 <span>constitutions</span>
-                <div></div>
+                <div className="button_container">
+                    <Button buttonSize={"btn--medium"}>View Constitutions</Button>
+                </div>
             </div>
             <div className='hero_right'></div>
         </div>
