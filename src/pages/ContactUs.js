7a6fc3b0fe8e471973bc40d2f6fb63797c358150
Caleb9083 <calebosam12@gmail.com>
2022-10-22 23:31:54 +0000

    Create Contact Form for Contact Page

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -1,17 +1,69 @@
-import React from 'react'
-import "./styles/ContactUs.css"
+import React from "react";
+import "./styles/ContactUs.css";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
 const ContactUs = () => {
     return (
         <div>
-            <div className="contact-section_top">
-                <div className="contact-section_top_contents">
+            <div className="contact_section_top">
+                <div className="contact_section_top_contents">
                     <h1>Contact Us</h1>
                     <p>Some text about who we are and what we do.</p>
                 </div>
             </div>
+            <div className="contact_section_main">
+                <div className="contact_section_main_left"></div>
+                <div className="contact_section_main_right">
+                    <Form
+                        style={{
+                            color: "#034078",
+                            boxShadow: "0.05rem 0.1rem 0.3rem -0.03rem rgba(0, 0, 0, 0.45)",
+                            padding: "20px",
+                            borderRadius: "15px"
+                        }}>
+                        <Form.Group className="mb-3">
+                            <Form.Label>Name</Form.Label>
+                            <Form.Control type="text" placeholder="John Doe" />
+                        </Form.Group>
+                        <Form.Group className="mb-3">
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control type="email" placeholder="name@example.com" />
+                        </Form.Group>
+                        <Form.Group
+                            className="mb-3"
+                            controlId="exampleForm.ControlTextarea1">
+                            <Form.Label>How can we help you</Form.Label>
+                            <Form.Control
+                                placeholder="Let us know how we can better assist you..."
+                                as="textarea"
+                                rows={3}
+                            />
+                        </Form.Group>
+                        <Form.Group>
+                            <Form.Text id="passwordHelpBlock" muted>
+                                By clicking Submit, I agree to be contacted at the phone number
+                                (by calling or text message) and email address provided with
+                                more information or offers about the Legal Companion project.
+                            </Form.Text>
+                        </Form.Group>
+                        <Form.Group style={{ marginTop: "5px" }}>
+                            <Button
+                                style={{
+                                    backgroundColor: "#034078",
+                                    color: "#fff",
+                                    border: "none",
+                                }}
+                                variant="primary"
+                                type="submit">
+                                Submit
+                            </Button>
+                        </Form.Group>
+                    </Form>
+                </div>
+            </div>
         </div>
-    )
-}
+    );
+};
 
-export default ContactUs
+export default ContactUs;
