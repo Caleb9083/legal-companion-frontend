e0a452beb5d6c6c2afab16cbfc4bb4d82e5e089e
Caleb9083 <calebosam12@gmail.com>
2022-10-26 22:12:59 +0000

    Remove unused code and variables in UserContext.js

--- src/context/userContext.js
+++ src/context/userContext.js
@@ -1,22 +1,7 @@
-import { createContext, useState, useMemo } from "react";
+import { createContext } from "react";
 
 export const UserContext = createContext({
     name: "",
     email: "",
     isLoggedIn: false
 });
-
-// const User = ({ children }) => {
-//     const [user, setUser] = useState({
-//         name: "",
-//         email: "",
-//         isLoggedIn: false
-//     });
-
-//     let value = useMemo(() => ({ user, setUser }), [user, setUser])
-
-//     return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
-// };
-
-
-// export default User;
