9672ed98fb90d5b85ed2d07a92c0130f6a87e2a1
Caleb9083 <calebosam12@gmail.com>
2022-10-26 14:57:31 +0000

    Handle change on inputs, Configure post for signup

--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -1,9 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles/LoginPage.css";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 const SignUpPage = () => {
+
+    const [signUpData, setSignUpData] = useState({
+        name: "",
+        email: "",
+        password: "",
+        confirmPassword: ""
+    })
+
+    const handleChange = (e) => {
+        const newSignUpData = { ...signUpData }
+        newSignUpData[e.target.name] = e.target.value;
+        setSignUpData(newSignUpData)
+    };
+
+    const { name, email, password, confirmPassword } = signUpData
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const { password, confirmPassword } = signUpData
+        if (password === confirmPassword) {
+            axios.post("http://localhost:5000/api/v2/auth/signup", {
+                ...signUpData
+            }).then((res) => {
+                console.log(res.data)
+                toast.success("Signed Up successfully")
+                setSignUpData({
+                    name: "",
+                    email: "",
+                    password: "",
+                    confirmPassword: ""
+                })
+            })
+        } else {
+            console.log("Password don't match")
+        }
+
+    }
     return (
 
         <div className="login_signup_container">
@@ -11,10 +50,14 @@ const SignUpPage = () => {
                 <p className="top_text_large">Don't have an account?</p>
                 <p>Sign up with your email and password</p>
             </div>
-            <Form style={{ color: "#034078" }}>
+            <Form onSubmit={(e) => handleSubmit(e)} style={{ color: "#034078" }}>
+                <Form.Group className="mb-3" controlId="formBasicUsername">
+                    <Form.Label>Username</Form.Label>
+                    <Form.Control name="name" value={name} onChange={(e) => { handleChange(e) }} type="text" placeholder="Enter username" />
+                </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
+                    <Form.Control name="email" value={email} onChange={(e) => { handleChange(e) }} type="email" placeholder="Enter email" />
                     <Form.Text className="text-muted">
                         We'll never share your email with anyone else.
                     </Form.Text>
@@ -22,12 +65,12 @@ const SignUpPage = () => {
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
+                    <Form.Control name="password" value={password} onChange={(e) => handleChange(e)} type="password" placeholder="Password" />
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Confirm Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
+                    <Form.Control name="confirmPassword" value={confirmPassword} onChange={(e) => { handleChange(e) }} type="password" placeholder="Password" />
                 </Form.Group>
                 <Button style={{ backgroundColor: "#034078", color: "#fff", border: "none", }} variant="primary" type="submit">
                     Submit
