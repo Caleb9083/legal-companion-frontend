46dca6abe889d19c37961eac2dc09a4c13da7325
Caleb9083 <calebosam12@gmail.com>
2022-12-14 15:32:07 +0000

    Update fieldname to connect with backend

--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -7,44 +7,44 @@ import { toast } from "react-toastify";
 import { BASE_URL } from "../url";
 
 const SignUpPage = () => {
-
     const [signUpData, setSignUpData] = useState({
         name: "",
         email: "",
         password: "",
-        confirmPassword: ""
-    })
+        passwordConfirm: "",
+    });
 
     const handleChange = (e) => {
-        const newSignUpData = { ...signUpData }
+        const newSignUpData = { ...signUpData };
         newSignUpData[e.target.name] = e.target.value;
-        setSignUpData(newSignUpData)
+        setSignUpData(newSignUpData);
     };
 
-    const { name, email, password, confirmPassword } = signUpData
+    const { name, email, password, passwordConfirm } = signUpData;
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        const { password, confirmPassword } = signUpData
-        if (password === confirmPassword) {
-            axios.post(`${BASE_URL}/api/v2/auth/signup`, {
-                ...signUpData
-            }).then((res) => {
-                toast.success("Signed Up successfully")
-                setSignUpData({
-                    name: "",
-                    email: "",
-                    password: "",
-                    confirmPassword: ""
-                })
+        axios
+            .post(`${BASE_URL}/api/v2/auth/signup`, {
+                ...signUpData,
             })
-        } else {
-            console.log("Password don't match")
-        }
+            .then((res) => {
 
-    }
+                if (res.data.status === "success") {
+
+                    toast.success("Signed Up successfully");
+                    setSignUpData({
+                        name: "",
+                        email: "",
+                        password: "",
+                        passwordConfirm: "",
+                    });
+                } else {
+                    toast.error(`${res.data.message}`)
+                }
+            });
+    };
     return (
-
         <div className="login_signup_container">
             <div className="login_top_text">
                 <p className="top_text_large">Don't have an account?</p>
@@ -53,11 +53,27 @@ const SignUpPage = () => {
             <Form onSubmit={(e) => handleSubmit(e)} style={{ color: "#034078" }}>
                 <Form.Group className="mb-3">
                     <Form.Label>Username</Form.Label>
-                    <Form.Control name="name" value={name} onChange={(e) => { handleChange(e) }} type="text" placeholder="Enter username" />
+                    <Form.Control
+                        name="name"
+                        value={name}
+                        onChange={(e) => {
+                            handleChange(e);
+                        }}
+                        type="text"
+                        placeholder="Enter username"
+                    />
                 </Form.Group>
                 <Form.Group className="mb-3">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control name="email" value={email} onChange={(e) => { handleChange(e) }} type="email" placeholder="Enter email" />
+                    <Form.Control
+                        name="email"
+                        value={email}
+                        onChange={(e) => {
+                            handleChange(e);
+                        }}
+                        type="email"
+                        placeholder="Enter email"
+                    />
                     <Form.Text className="text-muted">
                         We'll never share your email with anyone else.
                     </Form.Text>
@@ -65,19 +81,36 @@ const SignUpPage = () => {
 
                 <Form.Group className="mb-3">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control name="password" value={password} onChange={(e) => handleChange(e)} type="password" placeholder="Password" />
+                    <Form.Control
+                        name="password"
+                        value={password}
+                        onChange={(e) => handleChange(e)}
+                        type="password"
+                        placeholder="Password"
+                    />
                 </Form.Group>
 
                 <Form.Group className="mb-3">
                     <Form.Label>Confirm Password</Form.Label>
-                    <Form.Control name="confirmPassword" value={confirmPassword} onChange={(e) => { handleChange(e) }} type="password" placeholder="Password" />
+                    <Form.Control
+                        name="passwordConfirm"
+                        value={passwordConfirm}
+                        onChange={(e) => {
+                            handleChange(e);
+                        }}
+                        type="password"
+                        placeholder="Password"
+                    />
                 </Form.Group>
-                <Button style={{ backgroundColor: "#034078", color: "#fff", border: "none", }} variant="primary" type="submit">
+                <Button
+                    style={{ backgroundColor: "#034078", color: "#fff", border: "none" }}
+                    variant="primary"
+                    type="submit">
                     Submit
                 </Button>
             </Form>
         </div>
-    )
-}
+    );
+};
 
-export default SignUpPage
+export default SignUpPage;
