29355c5215f44470d4033c7c0dbec37c23775aef
Caleb9083 <calebosam12@gmail.com>
2022-12-15 09:58:07 +0000

    Authorization added for put requests

--- src/components/UpdateConstitutionModal.js
+++ src/components/UpdateConstitutionModal.js
@@ -1,96 +1,118 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 import Form from "react-bootstrap/Form";
 import axios from "axios";
 import { toast } from "react-toastify";
 import { BASE_URL } from "../url";
+import { UserContext } from "../context/userContext";
 
 const UpdateConstitutionModal = ({
-    handleClose,
-    handleShow,
-    constitutionId,
+  handleClose,
+  handleShow,
+  constitutionId,
 }) => {
-    const url = `${BASE_URL}/api/v2/constitutions/${constitutionId}`;
+  const { user } = useContext(UserContext);
+  const url = `${BASE_URL}/api/v2/constitutions/${constitutionId}`;
 
-    const [constitutionData, setConstitutionData] = useState({
-        name: "",
-        preamble: "",
-    });
+  const [constitutionData, setConstitutionData] = useState({
+    name: "",
+    preamble: "",
+  });
 
-    useEffect(() => {
-        axios.get(url).then((res) =>
-            setConstitutionData({
-                name: res.data.title,
-                preamble: res.data.preamble,
-            })
-        );
-    }, []);
-
-    const handleChange = (e) => {
-        const newConstitution = { ...constitutionData };
-        newConstitution[e.target.name] = e.target.value;
-        setConstitutionData(newConstitution);
-    };
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        axios
-            .put(url, {
-                title: constitutionData.name,
-                preamble: constitutionData.preamble,
-            })
-            .then((res) => {
-                handleClose();
-                toast.success("Constitution updated successfully");
-            })
-            .catch((err) => {
-                handleClose();
-                toast.error("Constitution not Updated!, Try again");
-            });
-    };
-    return (
-        <>
-            <Modal show={handleShow} onHide={handleClose}>
-                <Modal.Header closeButton>
-                    <Modal.Title style={{ color: "#034078" }}>Update constitution</Modal.Title>
-                </Modal.Header>
-                <Modal.Body style={{ color: "#034078" }}>
-                    <Form onSubmit={(e) => handleSubmit(e)}>
-                        <Form.Group className="mb-3">
-                            <Form.Label>Constitution Name</Form.Label>
-                            <Form.Control
-                                name="name"
-                                value={constitutionData.name}
-                                onChange={(e) => handleChange(e)}
-                                type="text"
-                                placeholder="Enter the name of the Constitution"
-                            />
-                        </Form.Group>
-
-                        <Form.Group className="mb-3">
-                            <Form.Label>Preamble</Form.Label>
-                            <Form.Control
-                                name="preamble"
-                                value={constitutionData.preamble}
-                                onChange={(e) => handleChange(e)}
-                                as="textarea"
-                                placeholder="Enter the Preamble"
-                            />
-                        </Form.Group>
-                        <Button style={{ backgroundColor: "#034078", color: "#fff", border: "none" }} variant="primary" type="submit">
-                            Submit
-                        </Button>
-                    </Form>
-                </Modal.Body>
-                <Modal.Footer>
-                    <Button variant="secondary" onClick={handleClose}>
-                        Discard
-                    </Button>
-                </Modal.Footer>
-            </Modal>
-        </>
+  useEffect(() => {
+    axios.get(url).then((res) =>
+      setConstitutionData({
+        name: res.data.title,
+        preamble: res.data.preamble,
+      })
     );
+  }, []);
+
+  const handleChange = (e) => {
+    const newConstitution = { ...constitutionData };
+    newConstitution[e.target.name] = e.target.value;
+    setConstitutionData(newConstitution);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .put(
+        url,
+        {
+          title: constitutionData.name,
+          preamble: constitutionData.preamble,
+        },
+        {
+          headers: {
+            ContentType: "application/json",
+            authorization: `Bearer ${user.token}`,
+          },
+        }
+      )
+      .then((res) => {
+        if (res.data.status === "success") {
+          handleClose();
+          toast.success("Constitution updated successfully");
+        } else {
+          handleClose();
+          toast.error(`${res.data.message}`);
+        }
+      });
+  };
+  return (
+    <>
+      <Modal show={handleShow} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title style={{ color: "#034078" }}>
+            Update constitution
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body style={{ color: "#034078" }}>
+          <Form onSubmit={(e) => handleSubmit(e)}>
+            <Form.Group className="mb-3">
+              <Form.Label>Constitution Name</Form.Label>
+              <Form.Control
+                name="name"
+                value={constitutionData.name}
+                onChange={(e) => handleChange(e)}
+                type="text"
+                placeholder="Enter the name of the Constitution"
+              />
+            </Form.Group>
+
+            <Form.Group className="mb-3">
+              <Form.Label>Preamble</Form.Label>
+              <Form.Control
+                name="preamble"
+                value={constitutionData.preamble}
+                onChange={(e) => handleChange(e)}
+                as="textarea"
+                placeholder="Enter the Preamble"
+              />
+            </Form.Group>
+            <Button
+              style={{
+                backgroundColor: "#034078",
+                color: "#fff",
+                border: "none",
+              }}
+              variant="primary"
+              type="submit"
+            >
+              Submit
+            </Button>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Discard
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
 };
 
 export default UpdateConstitutionModal;
--- src/components/UpdateSectionModal.js
+++ src/components/UpdateSectionModal.js
@@ -1,99 +1,121 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 import Form from "react-bootstrap/Form";
 import axios from "axios";
 import { toast } from "react-toastify";
 import { BASE_URL } from "../url";
+import { UserContext } from "../context/userContext";
 
 const UpdateSectionModal = ({
-    handleClose,
-    handleShow,
-    constitutionId,
-    chapterId,
-    sectionId,
+  handleClose,
+  handleShow,
+  constitutionId,
+  chapterId,
+  sectionId,
 }) => {
-    const url = `${BASE_URL}/api/v2/constitutions/${constitutionId}/chapters/${chapterId}/sections/${sectionId}`;
+  const { user } = useContext(UserContext);
+  const url = `${BASE_URL}/api/v2/constitutions/${constitutionId}/chapters/${chapterId}/sections/${sectionId}`;
 
-    const [sectionData, setSectionData] = useState({
-        title: "",
-        content: "",
-    });
+  const [sectionData, setSectionData] = useState({
+    title: "",
+    content: "",
+  });
 
-    useEffect(() => {
-        axios.get(url).then((res) =>
-            setSectionData({
-                title: res.data.title,
-                content: res.data.content,
-            })
-        );
-    }, []);
-
-    const handleChange = (e) => {
-        const newSection = { ...sectionData };
-        newSection[e.target.name] = e.target.value;
-        setSectionData(newSection);
-    };
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        axios
-            .put(url, {
-                title: sectionData.title,
-                content: sectionData.content,
-            })
-            .then((res) => {
-                handleClose();
-                toast.success("Section updated successfully");
-            })
-            .catch((err) => {
-                handleClose();
-                toast.error("Section not Updated!, Try again");
-            });
-    };
-    return (
-        <>
-            <Modal show={handleShow} onHide={handleClose}>
-                <Modal.Header closeButton>
-                    <Modal.Title style={{ color: "#034078" }}>Update Chapter Section</Modal.Title>
-                </Modal.Header>
-                <Modal.Body style={{ color: "#034078" }}>
-                    <Form onSubmit={(e) => handleSubmit(e)}>
-                        <Form.Group className="mb-3">
-                            <Form.Label>Section Title</Form.Label>
-                            <Form.Control
-                                name="title"
-                                value={sectionData.title}
-                                onChange={(e) => handleChange(e)}
-                                type="text"
-                                placeholder="Enter the title of the Section"
-                            />
-                        </Form.Group>
-
-                        <Form.Group className="mb-3">
-                            <Form.Label>Content</Form.Label>
-                            <Form.Control
-                                name="content"
-                                value={sectionData.content}
-                                onChange={(e) => handleChange(e)}
-                                as="textarea"
-                                style={{ height: "150px" }}
-                                placeholder="Enter the content for this section"
-                            />
-                        </Form.Group>
-                        <Button style={{ backgroundColor: "#034078", color: "#fff", border: "none" }} variant="primary" type="submit">
-                            Submit
-                        </Button>
-                    </Form>
-                </Modal.Body>
-                <Modal.Footer>
-                    <Button variant="secondary" onClick={handleClose}>
-                        Discard
-                    </Button>
-                </Modal.Footer>
-            </Modal>
-        </>
+  useEffect(() => {
+    axios.get(url).then((res) =>
+      setSectionData({
+        title: res.data.title,
+        content: res.data.content,
+      })
     );
+  }, []);
+
+  const handleChange = (e) => {
+    const newSection = { ...sectionData };
+    newSection[e.target.name] = e.target.value;
+    setSectionData(newSection);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .put(
+        url,
+        {
+          title: sectionData.title,
+          content: sectionData.content,
+        },
+        {
+          headers: {
+            ContentType: "application/json",
+            authorization: `Bearer ${user.token}`,
+          },
+        }
+      )
+      .then((res) => {
+        if (res.data.status === "success") {
+          handleClose();
+          toast.success("Section updated successfully");
+        } else {
+          handleClose();
+          toast.error(`${res.data.message}`);
+        }
+      });
+  };
+  return (
+    <>
+      <Modal show={handleShow} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title style={{ color: "#034078" }}>
+            Update Chapter Section
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body style={{ color: "#034078" }}>
+          <Form onSubmit={(e) => handleSubmit(e)}>
+            <Form.Group className="mb-3">
+              <Form.Label>Section Title</Form.Label>
+              <Form.Control
+                name="title"
+                value={sectionData.title}
+                onChange={(e) => handleChange(e)}
+                type="text"
+                placeholder="Enter the title of the Section"
+              />
+            </Form.Group>
+
+            <Form.Group className="mb-3">
+              <Form.Label>Content</Form.Label>
+              <Form.Control
+                name="content"
+                value={sectionData.content}
+                onChange={(e) => handleChange(e)}
+                as="textarea"
+                style={{ height: "150px" }}
+                placeholder="Enter the content for this section"
+              />
+            </Form.Group>
+            <Button
+              style={{
+                backgroundColor: "#034078",
+                color: "#fff",
+                border: "none",
+              }}
+              variant="primary"
+              type="submit"
+            >
+              Submit
+            </Button>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Discard
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
 };
 
 export default UpdateSectionModal;
